f63c20602f41ca1fbd8ef3f54151f1d562be7e6c
Zephyrus <2996362441@qq.com>
2023-03-07 01:02:11 +0800

    Assignment 1 Part 1

--- Assignment/PA-1/Part 1/perceptron.py
+++ Assignment/PA-1/Part 1/perceptron.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Perceptron(object):
 
     def __init__(self, n_inputs, max_epochs=1e2, learning_rate=1e-2):
@@ -10,15 +11,20 @@ class Perceptron(object):
             max_epochs: maximum number of training cycles.
             learning_rate: magnitude of weight changes at each training cycle
         """
-        
+        self.n = n_inputs
+        self.epochs = int(max_epochs)
+        self.lr = learning_rate
+        self.w = np.zeros(shape=self.n)
+
+    # noinspection PyShadowingBuiltins
     def forward(self, input):
         """
         Predict label from input 
         Args:
             input: array of dimension equal to n_inputs.
         """
-        return label
-        
+        return np.sign(self.w @ input)
+
     def train(self, training_inputs, labels):
         """
         Train the perceptron
@@ -26,3 +32,18 @@ class Perceptron(object):
             training_inputs: list of numpy arrays of training points.
             labels: arrays of expected output value for the corresponding point in training_inputs.
         """
+        for epoch in range(self.epochs):
+            for i in range(len(labels)):
+                predict = self.forward(training_inputs[i])
+                if predict != labels[i]:
+                    self.w += self.lr * (labels[i] * training_inputs[i])
+
+    def predict(self, inputs):
+        labels = []
+        for point in inputs:
+            labels.append(self.forward(point))
+        return np.array(labels)
+
+    def accuracy(self, test_data, test_labels):
+        predict = self.predict(test_data)
+        return np.sum(predict == test_labels) / len(test_labels)
